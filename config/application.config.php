092ea6f2c6a6ab6f68e387fea884a95e21a03581
Einar Valur <einarvalur@Einars-MacBook-Pro.local>
2013-08-11 16:57:55 +0000

    removed app Application from site

--- config/application.config.php
+++ config/application.config.php
@@ -6,6 +6,7 @@ return array(
         'Althingi',
         'Whiteboard',
         'Music',
+        'Maze',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
